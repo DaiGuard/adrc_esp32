952849823f2fc227d8b51fd8185c9d556f22cf81
DaiGuard <zenith.or.w@gmail.com>
2023-09-27 22:52:19 +0900

    シリアル通信がデッドロックしないように調整

--- src/Logging.h
+++ src/Logging.h
@@ -3,12 +3,15 @@
 
 #define MAX_LOG_STRING 128
 
-#define log_base(m, s, ...) do{ }while(0)
-// #define log_base(m, s, ...) do{ \
-//     char text[MAX_LOG_STRING]; \
-//     sprintf(text, s, ##__VA_ARGS__); \
-//     Serial.print(m); Serial.println(text); \
-//     }while(0)
+// #define log_base(m, s, ...) do{ }while(0)
+#define log_base(m, s, ...) do{ \
+        if(Serial.availableForWrite() > 32) \
+        { \
+            char text[MAX_LOG_STRING]; \
+            sprintf(text, s, ##__VA_ARGS__); \
+            Serial.print(m); Serial.println(text); \
+        } \
+    } while(0)
 
 #define log_info(s, ...) log_base("I: ", s, ##__VA_ARGS__)
 #define log_erro(s, ...) log_base("E: ", s, ##__VA_ARGS__)
